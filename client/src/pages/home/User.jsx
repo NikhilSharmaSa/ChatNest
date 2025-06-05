471f34354e5acdb9f92ce24941e5b9cfed33da22
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-06-05 10:48:06 +0530

    Add and test all users Api

--- client/src/pages/home/User.jsx
+++ client/src/pages/home/User.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function User() {
+function User({user}) {
     return (
         <div className='bg-base-100 hover:scale-105 scroll-smooth hover:transition-all  rounded-4xl cursor-pointer px-2 py-1 mb-2'>
 
@@ -9,12 +9,12 @@ function User() {
      <div className='flex gap-5 '>
         <div className="avatar avatar-online">
   <div className="w-12 rounded-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    <img src={user.avatar} />
   </div>
 </div>
 <div>
-    <h2 className='line-clamp-1'>Nikhil Sharma</h2>
-    <h4 className='text-xs'>nik@773</h4>
+    <h2 className='line-clamp-1'>{user.fullname}</h2>
+    <h4 className='text-xs'>{user.username}</h4>
 </div>
      </div>
       </div>
--- client/src/pages/home/UserSideBar.jsx
+++ client/src/pages/home/UserSideBar.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 import { FaSearch } from 'react-icons/fa';
 import User from './User';
 import Profile from './profile';
+import { useEffect } from 'react';
+import axios from "axios"
+import { useState } from 'react';
+
+
 
 function UserSideBar() {
+
+const [users,setUsers]=useState([])
+
+
+
+
+    const getUsers=async()=>{
+   const users= await  axios.get(`/api/v1/user/getAllUsers`,{withCredentials:true})
+console.log(users.data.data)
+setUsers(users.data.data)
+    }
+useEffect(()=>{
+ getUsers()
+},[])
+
     return (
         <div className='min-w-[18em] bg-base-300 max-h-screen flex flex-col'>
             <div className='text-center p-2 text-2xl font-bold'>
@@ -18,39 +38,11 @@ function UserSideBar() {
             </div>
 
             <div className='flex-1 overflow-auto px-4'>
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
-                <User />
+               {
+                users.map((user)=>(
+<User user={user}/>
+                ))
+               }
             </div>
 
             <div className='bg-black'>
