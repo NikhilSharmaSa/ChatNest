280d198dc69fe81d30c2ffe623bb81302da88263
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-06-08 00:00:14 +0530

    User cors

--- server/app.js
+++ server/app.js
@@ -3,12 +3,20 @@ import userRouter from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
 const app=express()
+app.use(cors(
+{
+
+    origin:[`http://localhost:5173`],
+    credentials:true
+
+}
+))
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
 app.use('/public', express.static('public'));
 
-app.use(cors())
+
 
 
 app.use('/api/v1/user', userRouter)
